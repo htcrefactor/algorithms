3f093de0b71016e832b76104de2af4bd71d5990c
Hyung-Taik Choi <htc.refactor@gmail.com>
2020-03-09 18:35:56 +0900

    Different solution code for baekjoon/MinMax. Runtime error thrown when submitted to judging system.

--- 1d-arrays/baekjoon/MinMax (10818)/solution.cpp
+++ 1d-arrays/baekjoon/MinMax (10818)/solution.cpp
@@ -1,3 +1,33 @@
+//Different solution using a 1d-array.
+#include <iostream> 
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    vector<int> numbers{1000000, 0};
+    int cases = 0;
+    cin >> cases;
+
+    int min = 1000000;
+    int max = -1000000;
+    for(int i = 0 ; i < cases ; i++) {
+        cin >> numbers[i];
+        if(numbers[i] > max) {
+            max = numbers[i];
+        }
+
+        if(numbers[i] < min) {
+            min = numbers[i];
+        }
+    }
+
+    cout << min << " " << max;
+
+    return 0;    
+}
+
+/*
 #include <iostream>
 
 using namespace std;
@@ -26,3 +56,4 @@ int main(int argc, char* argv[]) {
 
     return 0;
 }
+*/
