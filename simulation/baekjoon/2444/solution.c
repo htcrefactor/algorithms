509cfadd4645c708de1f05e933074a5a696e06a3
Hyung-Taik Choi <htc.refactor@gmail.com>
2021-05-20 13:47:16 +0900

    Solution code for baekjoon/2444

---
+++ simulation/baekjoon/2444/solution.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void spaces(int count) {
+    while(count--) {
+        printf(" ");
+    }
+}
+
+void stars(int count) {
+    while(count--) {
+        printf("*");
+    }
+}
+
+int main() {
+    int n = -1;
+    scanf("%d", &n);
+
+    for(int row = 1 ; row <= n ; row++) {
+        spaces(n - row);
+        stars(2 * row - 1);
+        printf("\n");
+    }
+
+    for(int row = n-1 ; 1 <= row ; row--) {
+        spaces(n - row);
+        stars(2 * row - 1);
+        printf("\n");
+    }
+
+    return 0;
+}
