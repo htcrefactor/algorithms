91a16fcfc9fb3d951d4409bc416ca0179a2a6142
Hyung-Taik Choi <htc.refactor@gmail.com>
2021-07-22 19:59:59 +0900

    Solution code for baekjoon/1463

---
+++ math/baekjoon/1463/solution.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int min(int new, int old) {
+    if (old == 0) return new;
+    
+    if (new < old) return new;
+    else return old;
+}
+
+int main() {
+    int n = 0;
+    scanf("%d", &n);
+    
+    int* array = (int*)malloc(sizeof(int) * (n + 1));
+    for (int index = 0 ; index < n + 1 ; index++) {
+        array[index] = 0;
+    }
+    
+    array[1] = 1;
+
+    int index = 1;
+    while (index < n) {
+        if (array[index] != 0) {
+            if (index * 3 <= n) array[index * 3] = min(array[index] + 1, array[index * 3]);
+            if (index * 2 <= n) array[index * 2] = min(array[index] + 1, array[index * 2]);
+            if (index + 1 <= n) array[index + 1] = min(array[index] + 1, array[index + 1]);
+        }
+        
+        index = index + 1;
+    }
+    
+    printf("%d\n", array[n] - 1);
+    
+    free(array);
+    
+    return 0;
+}
